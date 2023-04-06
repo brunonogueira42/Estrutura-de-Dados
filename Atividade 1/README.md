ba18dbbe4e848d48b9b189c37710fab84758bdbd
Bruno Nogueira <bruno.nogueira.bruno@gmail.com>
2023-04-06 16:55:15 -0300

    Atividade 1

---
+++ Atividade 1/README.md
@@ -0,0 +1,8 @@
+## Atividade 1
+
+Implemente o TAD PontoCartesiano em Java
+
+- Crie um método construtor;
+- Getters e Setters;
+- Crie um método que calcule a distância euclidiana entre dois pontos;
+- Teste esse TAD em uma classe Main.
---
+++ Atividade 1/src/PontoCartesiano.java
@@ -0,0 +1,30 @@
+public class PontoCartesiano {
+    private double x;
+    private double y;
+
+    public PontoCartesiano(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+
+    public double Euclidiana(PontoCartesiano ponto1, PontoCartesiano ponto2) {
+        double quadx, quady, soma, raiz;
+
+        quadx = (ponto1.getX() - ponto2.getX()) * (ponto1.getX() - ponto2.getX());
+        quady = (ponto1.getY() - ponto2.getY()) * (ponto1.getY() - ponto2.getY());
+        soma = quadx + quady;
+        raiz = Math.sqrt(soma); // Queria fazer sem usar o método Math.sqrt, mas não consegui.
+
+        return raiz;
+    }
+
+    public String toString() {
+        return "("+x+","+y+")";
+    }
+}
