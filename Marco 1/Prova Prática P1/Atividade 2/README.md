e82c6508dbbf77e389d6d4cbaf2e180fa1d68397
Bruno Nogueira <bruno.nogueira.bruno@gmail.com>
2023-05-19 22:08:48 -0300

    P1 Questão 2

---
+++ Marco 1/Prova Prática P1/Atividade 2/README.md
@@ -0,0 +1,3 @@
+## Questão 2
+
+O método Selection Sort, abaixo, sofreu um pequeno ajuste: passou a trabalhar com a seleção do maior valor, colocando-o na última posição; do segundo maior valor colocando-o na penúltima posição e, assim, sucessivamente. Complete-o!
---
+++ Marco 1/Prova Prática P1/Atividade 2/src/App.java
@@ -0,0 +1,30 @@
+public class App {
+
+    float valor[];
+
+    public void selectionSort() {
+        for (int i = valor.length; i >= 1; i--) {
+           int maior = this.posMaior(i);
+              if (maior != i) 
+                 this.troca (i, maior);
+           }
+    }
+     /* Métodos auxiliares: */
+    private int posMaior(int fim) {
+        int maior = fim;
+        for (int i = fim-1; i >= 0; i--)
+           if (valor[i] > valor[maior])
+              maior = i;
+        return maior;
+    }
+    private void troca(int a, int b) {
+        float aux;
+        aux = valor[a];
+        valor[a] = valor[b];
+        valor[b] = aux;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
